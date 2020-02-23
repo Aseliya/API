d50634bb86cc4c573461313d796d5cccd78fbebe
Асель Кенжебекова <aselkenzebekova@MacBook-Air-Asel.local>
2020-02-24 00:57:48 +0600

    Add gem shoulda-matchers,add validation to User model and write tests on it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :news
+  has_many :news, dependent: :destroy
+  validates :email, presence: true
+  validates :password_digest, presence: true
   has_secure_password
 end
