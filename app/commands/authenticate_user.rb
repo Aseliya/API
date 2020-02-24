719efd7af48aa574b015002c3ec95e00bad1f860
Асель Кенжебекова <aselkenzebekova@MacBook-Air-Asel.local>
2020-02-25 01:45:14 +0600

    Add Errors model and fix auth test

--- app/commands/authenticate_user.rb
+++ app/commands/authenticate_user.rb
@@ -19,8 +19,7 @@ class AuthenticateUser
   def user
     user = User.find_by_email(email)
     return user if user&.authenticate(password)
-
-    errors.add :user_authentication, "invalid credentials"
+    raise AuthenticationError, "invalid credentials"
     nil
   end
 end
