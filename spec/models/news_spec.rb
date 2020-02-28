04fc90ac2c260573a6af3fb08bf00e5f02d439e3
Асель Кенжебекова <aselkenzebekova@MacBook-Air-Asel.local>
2020-02-28 19:19:27 +0600

    Fix tests on user, news and favorite models

--- spec/models/news_spec.rb
+++ spec/models/news_spec.rb
@@ -4,5 +4,6 @@ require "rails_helper"
 RSpec.describe News, type: :model do
   describe "associations" do
     it { should belong_to(:user) }
+    it { should have_many(:favorites) }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ require "rails_helper"
 RSpec.describe User, type: :model do
   describe "associations" do
     it { should have_many(:readed_news) }
+    it { should have_many(:favorites) }
   end
   describe "validations" do
     it { should validate_presence_of(:email) }
